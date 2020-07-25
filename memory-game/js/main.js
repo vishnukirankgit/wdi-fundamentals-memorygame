a3ac44f8b9367c6ae140aee5a763e536b3d7a08d
vishnukirankgit <kurrivishnu@johndeere.com>
2020-07-25 16:12:17 -0500

    created game logic. Folder with underscore is before JS and the one with hyphen is from JS class

---
+++ memory-game/js/main.js
@@ -0,0 +1,8 @@
+
+let cardOne = "king";
+let cardTwo = "king";
+let cardThree = "queen";
+let cardFour = "queen";
+
+console.log("User Flipped " + cardOne);
+console.log("User Flipped " +cardThree);
